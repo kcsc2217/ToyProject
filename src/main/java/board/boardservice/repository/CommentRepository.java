1e33d315771157251dddac8a8d22c2fcb1f6dd23
kcsc2217 <k12002@nate.com>
2024-03-28 21:20:57 +0900

    각 리포지토리 구현 완료

---
+++ src/main/java/board/boardservice/repository/CommentRepository.java
@@ -0,0 +1,33 @@
+package board.boardservice.repository;
+
+import board.boardservice.domain.Comment;
+import board.boardservice.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+
+public class CommentRepository {
+
+    private final EntityManager em;
+
+
+    public void save(Comment comment) {
+        em.persist(comment);
+    }
+
+    public Comment findOne(Long id) {
+        return em.find(Comment.class, id);
+
+    }
+
+    public List<Comment> findAll(){
+        return em.createQuery("select c from Comment c", Comment.class)
+                .getResultList();
+    }
+}
---
+++ src/main/java/board/boardservice/repository/PostRepository.java
@@ -0,0 +1,32 @@
+package board.boardservice.repository;
+
+import board.boardservice.domain.Member;
+import board.boardservice.domain.Post;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+
+    private final EntityManager em;
+
+
+    public void save(Post post) {
+        em.persist(post);
+    }
+
+    public Post findOne(Long id) {
+        return em.find(Post.class, id);
+
+    }
+
+    public List<Post> findAll(){
+        return em.createQuery("select p from Post p", Post.class)
+                .getResultList();
+    }
+
+}
